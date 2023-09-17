adeb5addb1397b03a6a188df9f20f0de34a6683c
MD HUZAIFA  ANSARI <96833478+Huzaifa-Ansari222@users.noreply.github.com>
2023-09-18 00:30:49 +0530

    Add files via upload

---
+++ Functionss.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package functionss;
+
+public class Functionss {
+
+    public static void main(String[] args) {
+        hiName();
+        hiSecondName();
+    }
+   static void hiName(){
+        System.out.print("hi am huzz");
+   }
+        static void SecondName(){
+            System.out.println(" ansari");
+        }
+      
+
+static int age(int )
+{
+
+}
+}
