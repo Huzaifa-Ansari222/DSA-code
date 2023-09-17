4c049ae4e45efdfe0ff160067e57458257d0098a
MD HUZAIFA  ANSARI <96833478+Huzaifa-Ansari222@users.noreply.github.com>
2023-09-18 00:32:52 +0530

    Add files via upload

---
+++ StringWithRetrnNarge.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stringwithretrnnarge;
+
+/**
+ *
+ * @author huzai
+ */
+public class StringWithRetrnNarge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      String message= greet();
+        System.out.println(message);
+    }
+    
+   static  String greet(){
+       
+       String greeting="hi am huzaifa";
+       return greeting;
+   }
+    
+}
