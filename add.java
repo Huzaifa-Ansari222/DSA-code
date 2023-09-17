11c48db7adfa0d01ce08e1d14d5587da6765f257
MD HUZAIFA  ANSARI <96833478+Huzaifa-Ansari222@users.noreply.github.com>
2023-09-18 00:33:13 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,16 @@
+
+package sumof;
+
+public class Sumof { 
+   //  Scanner sc=new Scanner(System.in);
+    //int
+    System.out.println("hi")
+}
+
+    public static void main(String[] args)
+{
+        
+       Sumof();
+    
+}
+
