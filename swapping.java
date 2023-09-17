cbd9bc7ab81bf6e395adeee454c41f2bcb7a5d01
MD HUZAIFA  ANSARI <96833478+Huzaifa-Ansari222@users.noreply.github.com>
2023-09-18 00:31:19 +0530

    Add files via upload

---
+++ swapping.java
@@ -0,0 +1,18 @@
+public class swapping {
+    public static void main(String[] args) {
+int a=9
+        int b=3
+   willChange(a,b);
+        System.out.println(+a" "+b);
+    }
+    static void willChange(int a,int b){
+int a,b;
+int temp
+        a=temp;
+b=a;
+temp=b;
+    }
+
+
+
+}
