973e77b851453176c42cdd1e47787de6c68d98c8
MD HUZAIFA  ANSARI <96833478+Huzaifa-Ansari222@users.noreply.github.com>
2023-09-18 00:32:04 +0530

    Add files via upload

---
+++ RetrunWIthArgs.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package retrunwithargs;
+/**
+ *
+ * @author huzai
+ */
+public class RetrunWIthArgs {
+        public static void main(String[] args) {
+int ans;
+            ans = add(2,3);
+System.out.print(ans);
+	}
+
+	static int add(int a,int b){
+     //Scanner in=new Scanner(System.in);
+    
+     int sum=a+b;
+     return sum;
+	}
+}
+
