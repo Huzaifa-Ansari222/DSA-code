f13873ca06dd692bec53a458d55ac38370721a82
MD HUZAIFA  ANSARI <96833478+Huzaifa-Ansari222@users.noreply.github.com>
2023-09-18 00:32:28 +0530

    Add files via upload

---
+++ ReturnString.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package returnstring;
+
+/**
+ *
+ * @author huzai
+ */
+public class ReturnString {
+
+    public static void main(String[] args) {
+        String tmp=willReturn();
+        System.out.println(tmp);
+    }
+static String willReturn(){
+    String name="huzzaif asnaei";
+    return name; //name>huzaifa asnaeri
+}    
+}
