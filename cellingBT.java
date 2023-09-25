bc234678934ae78a2603da5db8a1b3829b5e268d
MD HUZAIFA  ANSARI <96833478+Huzaifa-Ansari222@users.noreply.github.com>
2023-09-25 22:01:54 +0530

    Update cellingBT.java

--- cellingBT.java
+++ cellingBT.java
@@ -8,7 +8,9 @@ public class cellingBT {
     static int function(int []arr , int target){
         int start=0;
         int end = arr.length - 1;
-
+        if(target>arr[arr.length-1]){
+            return -1;
+        }
         while(start<= end){
             int mid= start + (end - start ) / 2;
             if(target < arr[mid]){
