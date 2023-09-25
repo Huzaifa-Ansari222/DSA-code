e0f43099a2a8b3ad2ff8aed9dfe5a45ad59a964e
MD HUZAIFA  ANSARI <96833478+Huzaifa-Ansari222@users.noreply.github.com>
2023-09-25 22:00:56 +0530

    Update floorBT.java

--- floorBT.java
+++ floorBT.java
@@ -1,13 +1,16 @@
 public class floorBT {
     public static void main(String[] args) {
         int []arr={1,8,10,30,42,51};
-        int target=20;
+        int target=0;
         int ans=function(arr ,target);
         System.out.println(ans);
     }
     static int function(int []arr , int target){
         int start=0;
         int end = arr.length - 1;
+        if(target < arr[arr.length-1]){
+            return -1;
+        }
 
         while(start<= end){
             int mid= start + (end - start ) / 2;
