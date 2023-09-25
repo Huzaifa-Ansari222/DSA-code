351c54923232a22898d77adad9780e2e240f9473
MD HUZAIFA  ANSARI <96833478+Huzaifa-Ansari222@users.noreply.github.com>
2023-09-25 21:48:39 +0530

    Add files via upload

---
+++ floorBT.java
@@ -0,0 +1,27 @@
+public class floorBT {
+    public static void main(String[] args) {
+        int []arr={1,8,10,30,42,51};
+        int target=20;
+        int ans=function(arr ,target);
+        System.out.println(ans);
+    }
+    static int function(int []arr , int target){
+        int start=0;
+        int end = arr.length - 1;
+
+        while(start<= end){
+            int mid= start + (end - start ) / 2;
+            if(target < arr[mid]){
+                end=mid-1;
+
+            }
+            else if (arr[mid]<target) {
+                start=mid+1;
+            }
+            else {
+                return mid;//return answer
+            }
+        }
+        return end;//when nothing exceute means not found
+    }
+}
